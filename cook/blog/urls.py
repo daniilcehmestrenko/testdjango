a20b654ba1dbe059ac402a1da6e2796da2eda655
Danil <daniilcehmestrenko@icloud.com>
2022-09-07 15:07:23 +0300

    add blog

---
+++ cook/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('comment/<int:pk>/', views.CreateComment.as_view(), name='create_comment'),
+    path('<slug:slug>/<slug:post_slug>', views.PostDetail.as_view(), name='post_single'),
+    path('<slug:slug>/', views.PostListView.as_view(), name='post_list'),
+    path('', views.HomeView.as_view(), name='home'),
+]
